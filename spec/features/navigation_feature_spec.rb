d4f1aaccc098cb1bd4b7ef16f8e7ff8e1fa0d72f
Nate Dalo <nate.dalo@yahoo.com>
2015-04-08 09:11:12 -0500

    Refactor navigation_feature_spec.rb

--- spec/features/navigation_feature_spec.rb
+++ spec/features/navigation_feature_spec.rb
@@ -4,38 +4,32 @@ describe 'navigation' do
   let!(:account) { create(:account_with_schema) }
   let(:user) { account.owner }
 
-  before { set_subdomain(account.subdomain) }
-  
-  it 'allows navigation to the configuration page' do
+  before do 
+    set_subdomain(account.subdomain)
     sign_user_in(user)
     page.find(:css, 'a[href="/pages/index"]').click
+  end
+  
+  it 'allows navigation to the configuration page' do
     expect(page).to have_content("Configuration")
   end
 
   it 'allows navigation to add new client' do
-    sign_user_in(user)
-    page.find(:css, 'a[href="/pages/index"]').click
     click_link("Add New Client")
     expect(page).to have_content("New Client")
   end
 
   it 'allows navigation to add new product' do
-    sign_user_in(user)
-    page.find(:css, 'a[href="/pages/index"]').click
     click_link("Add New Product")
     expect(page).to have_content("New Product")
   end
 
   it 'allows navigation to add new status' do
-    sign_user_in(user)
-    page.find(:css, 'a[href="/pages/index"]').click
     click_link("Add New Status")
     expect(page).to have_content("New Status")
   end
 
   it 'allows navigation to add new priority' do
-    sign_user_in(user)
-    page.find(:css, 'a[href="/pages/index"]').click
     click_link("Add New Priority")
     expect(page).to have_content("New Priority")
   end      
