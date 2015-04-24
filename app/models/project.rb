791faed68f11891285dbde1d0cef396a293e26ff
Nate Dalo <nate.dalo@yahoo.com>
2015-04-24 12:08:49 -0500

    Add index and edit actions for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,15 @@ class Project < ActiveRecord::Base
   has_many :tasks
   belongs_to :client
   belongs_to :user
+
+  def self.time_spent(project)
+    time_spent = 0
+    @all_tasks = project.tasks
+    @all_tasks.each do |task|
+      task.task_details.each do |td|
+        time_spent += td.time_spent
+      end
+    end
+    time_spent
+  end
 end
