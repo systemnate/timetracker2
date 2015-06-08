539fafd9e7f6a4d290a2b8d094f248048f228234
Nate Dalo <nate.dalo@yahoo.com>
2015-06-08 16:19:03 -0500

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,8 +30,8 @@ class ApplicationController < ActionController::Base
     if current_account
       Apartment::Tenant.switch!(current_account.subdomain)
     else
-      redirect_to root_url(subdomain: nil)
-      #render 'welcome/index'
+      #redirect_to root_url(subdomain: false)
+      render 'welcome/index'
     end
   end
 
