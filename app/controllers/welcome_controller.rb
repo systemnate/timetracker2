385eac30d53fecc9401c3d6bceb86bf37d853b1a
Nate Dalo <nate.dalo@yahoo.com>
2015-06-11 13:21:35 -0500

    Add logo and try to prevent layout from loading

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   skip_before_filter :authenticate_user!, only: :index
-  
+  layout 'application', except: [:index]
   def index
     render :layout => false  
   end
