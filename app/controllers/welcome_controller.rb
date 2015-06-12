fca6427a8ec2e11cfb2650c9fe4ce44962c5800a
Nate Dalo <nate.dalo@yahoo.com>
2015-06-12 13:55:40 -0500

    remove layout for landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_filter :authenticate_user!, only: :index
   layout false
   def index
-    #render :layout => false  
+    render :layout => false  
   end
 end
