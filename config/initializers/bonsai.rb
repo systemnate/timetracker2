00f37b054e671025196e6a227b1c7f876e54bd14
Nate Dalo <nate.dalo@yahoo.com>
2015-06-15 17:39:16 -0500

    Remove background image for task

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,7 +1,3 @@
 if ENV['BONSAI_URL']
-  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
-  BONSAI_INDEX_NAME = ENV['BONSAI_URL'][/[^\/]+$/]
-else
-  app_name = Rails.application.class.parent_name.underscore.dasherize
-  BONSAI_INDEX_NAME = "#{app_name}-#{Rails.env}"
+     Searchkick.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
 end
