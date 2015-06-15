1b6f6009aaa5a4c940050e18615de691c1bd438b
Nate Dalo <nate.dalo@yahoo.com>
2015-06-15 17:28:08 -0500

    Change Bonsai initializer

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,7 +1,3 @@
-# config/initializers/bonsai.rb
-
-require 'elasticsearch/model'
-
 if ENV['BONSAI_URL']
   Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
   BONSAI_INDEX_NAME = ENV['BONSAI_URL'][/[^\/]+$/]
