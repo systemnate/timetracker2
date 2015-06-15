aa6f5450fd81202e25954097d2e50122ddab0de2
Nate Dalo <nate.dalo@yahoo.com>
2015-06-15 17:26:58 -0500

    Change Bonsai initializer

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,3 +1,11 @@
+# config/initializers/bonsai.rb
+
+require 'elasticsearch/model'
+
 if ENV['BONSAI_URL']
-     Searchkick.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
+  BONSAI_INDEX_NAME = ENV['BONSAI_URL'][/[^\/]+$/]
+else
+  app_name = Rails.application.class.parent_name.underscore.dasherize
+  BONSAI_INDEX_NAME = "#{app_name}-#{Rails.env}"
 end
