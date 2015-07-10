7ff41fa1f4090dc02a82e5b1b27e3a13018e2706
Nate Dalo <nate.dalo@yahoo.com>
2015-07-10 11:34:34 -0500

    Show created by and date when displaying task details

--- app/helpers/task_details_helper.rb
+++ app/helpers/task_details_helper.rb
@@ -1,10 +1,24 @@
 module TaskDetailsHelper
   def show_time_spent(task_detail)
     if task_detail.time_spent < 60
-      content_tag(:p, "Time Spent: " + pluralize(task_detail.time_spent, "minute"))
+      content_tag(:p, show_created_by(task_detail) + " on " + show_date_created(task_detail) + " - Time Spent: " + pluralize(task_detail.time_spent, "minute") + ".")
     else
-      content_tag(:p, "Time Spent: " + pluralize(number_with_precision(task_detail.time_spent / 60.0, precision: 2), "hour"))
+      content_tag(:p, show_created_by(task_detail) + " on " + show_date_created(task_detail) + " - Time Spent: " + pluralize(number_with_precision(task_detail.time_spent / 60.0, precision: 2), "hour") + ".")
     end
+  end
+
+  def show_created_by(task_detail)
+    user = User.find_by(id: task_detail.user_id)
+    if user.nil?
+      task_detail.update(user_id: current_user.id)
+      user = User.find_by(id: task_detail.user_id)
+    end
+    name = user.name
+    "Created by: " + user.name
+  end
+
+  def show_date_created(task_detail)
+    task_detail.created_at.getlocal.to_s + " (" + time_ago_in_words(task_detail.created_at.getlocal) + " ago)"
   end
 
   def show_detail(task_detail)
