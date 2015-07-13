3abfa17f59ffb8c1213eb9d43b5f3400102f645d
Nate Dalo <nate.dalo@yahoo.com>
2015-07-13 12:53:54 -0500

    Add new abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,8 +8,14 @@ class Ability
     elsif user.regular?
       can :read, :all
       can :create, :all
-      can :update, :all
       can :update, TaskDetail, user_id: user.id
+      can :update, Task
+      can :update, Client
+      can :update, Priority
+      can :update, Product
+      can :update, Project
+      can :update, Status
+      can :update, User
     elsif user.viewer?
       can :read, :all
     end
