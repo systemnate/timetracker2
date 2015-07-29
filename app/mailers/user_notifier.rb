08936b8b5400e633f0a32e5ff3a2ffc7b6dac222
Nate Dalo <nate.dalo@yahoo.com>
2015-07-29 12:39:49 -0500

    Restyle task update message

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -4,22 +4,26 @@ class UserNotifier < ApplicationMailer
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
-    mail( :to => @user.email,
-    :subject => 'Thanks for signing up for our amazing app' )
+    mail( to: @user.email,
+    subject: 'Thanks for signing up for our amazing app' )
   end
 
   def send_status_update(task, current_account)
+    @task = task
+    @task_details = task.task_details
     if !task.notify_email.blank?
       emails_to_send = task.notify_email.split(',')
       render :layout => false
       @current_account = current_account.subdomain
       @task = task
       @task_id = task.id
+      @task_alternate_id = task.alternate_id
       @task_title = task.title
       @task_status = task.status.name
       @task_priority = task.priority.name
       @task_product = task.product.name
       @task_client = task.client.name
+      @task_date = task.created_at
       @user = User.find(task.assigned_to)
       @user_name = @user.name
       @user_email = @user.email
