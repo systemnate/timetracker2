96899d4b4eb6e3f250d4417e9ff45b37b87eeaa2
Nate Dalo <nate.dalo@yahoo.com>
2015-08-04 17:12:44 -0500

    Add validations for Todo Lists

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,6 +1,8 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
 
+  validates :content, presence: true
+
   def completed?
     !completed_at.blank?
   end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,5 @@
 class TodoList < ActiveRecord::Base
   has_many :todo_items, dependent: :destroy
+
+  validates :title, presence: true
 end
