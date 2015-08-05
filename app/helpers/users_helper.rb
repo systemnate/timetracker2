315455eb841a2f5294b4443cc4e444f97abf55e3
Nate Dalo <nate.dalo@yahoo.com>
2015-08-05 15:11:29 -0500

    Change contact gravatar to square

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -11,11 +11,11 @@ module UsersHelper
   def gravatar_email(contact)
     if contact.email.blank?
       gravatar_url = "https://secure.gravatar.com/avatar/0"
-      image_tag(gravatar_url, class: 'gravatar img-circle', size: '75x75')
+      image_tag(gravatar_url, class: 'gravatar', size: '75x75')
     else  
       gravatar_id = Digest::MD5::hexdigest(contact.email.downcase)
       gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-      image_tag(gravatar_url, class: 'gravatar img-circle', size: '75x75')
+      image_tag(gravatar_url, class: 'gravatar', size: '75x75')
     end
   end
 
