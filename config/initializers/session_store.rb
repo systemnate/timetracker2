0b2a46afa82fb2f9cff469bd205c8e1efbf24b95
Nate Dalo <nate.dalo@yahoo.com>
2015-08-07 13:09:06 -0500

    Add devise initializer for IE compatibility

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_timetracker2_session'
+# Rails.application.config.session_store :cookie_store, key: '_timetracker2_session'
+
+Rails.application.config.session_store :cookie_store, key: '_timetracker2_session', domain: {
+  production: '.taskclash.com',
+  development: '.taskclash.dev'
+}.fetch(Rails.env.to_sym, :all)
