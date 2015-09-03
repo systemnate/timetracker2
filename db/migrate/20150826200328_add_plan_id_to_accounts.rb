558500c26bc252dea5e59f4547a5577120deab2e
Nate Dalo <nate.dalo@yahoo.com>
2015-09-03 13:24:22 -0500

    Undo stripe migrations

--- db/migrate/20150826200328_add_plan_id_to_accounts.rb
+++
@@ -1,5 +0,0 @@
-class AddPlanIdToAccounts < ActiveRecord::Migration
-  def change
-    add_column :accounts, :plan_id, :string
-  end
-end
--- db/migrate/20150826200955_add_stripe_to_accounts.rb
+++
@@ -1,5 +0,0 @@
-class AddStripeToAccounts < ActiveRecord::Migration
-  def change
-    add_column :accounts, :stripe_customer_token, :string
-  end
-end
