ba7b53c1c9720801b3fc8768e03e4e866fb71b82
Nate Dalo <nate.dalo@yahoo.com>
2016-02-24 10:10:34 -0600

    Decompose Search#find_tasks

--- app/models/search.rb
+++ app/models/search.rb
@@ -21,6 +21,10 @@ private
     else
       tasks = Task.includes(:status, :client, :product, :priority, :priority => :color, :status => :color, :product => :color).order("created_at DESC")
     end
+    search_keywords(tasks)
+  end
+
+  def search_keywords(tasks)
     tasks = tasks.where("id = ?", task_id) if task_id.present?
     tasks = tasks.where(status_id: status_id) if status_id.present?
     tasks = tasks.where(product_id: product_id) if product_id.present?
@@ -28,8 +32,8 @@ private
     tasks = tasks.where(client_id: client_id) if client_id.present?
     tasks = tasks.where(assigned_to: assigned_to) if assigned_to.present?
     tasks = tasks.where(created_by: created_by) if created_by.present?
-    tasks = tasks.where("alternate_id ilike ?", "%#{alternate_id}%") if alternate_id.present?
-    tasks = tasks.where("title ilike ?", "%#{title}%") if title.present?
+    tasks = tasks.where("alternate_id like ?", "%#{alternate_id}%") if alternate_id.present?
+    tasks = tasks.where("title like ?", "%#{title}%") if title.present?
     tasks = tasks.where(billable: billable) if billable.present?
     tasks = tasks.where(project_id: project_id) if project_id.present?
     tasks
